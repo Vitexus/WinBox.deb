711d005072c0add70efa56f3e8d2ea0b118f6117
vitex <vitex@hosting.vitexsoftware.cz>
2018-10-02 13:40:20 +0200

    Update package version durring successfull update

--- downloadExe.sh
+++ downloadExe.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-wget https:`curl -s https://mikrotik.com/download | sed 's/\"/\n/g'  | grep winbox.exe`
-
+DWLURL=`curl -s https://mikrotik.com/download | sed 's/\"/\n/g'  | grep winbox.exe`
+VERSION=`echo $DWLURL | awk -F/ '{print $6}'`
+dch --upstream --distribution stable --newversion $VERSION "package update with winbox.exe $VERSION"
+git commit -m "package update with winbox.exe $VERSION" debian/changelog
+wget https:$DWLURL
 
