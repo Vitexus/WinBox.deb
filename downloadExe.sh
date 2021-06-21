90a65b0d164acb754919a356104313e245f78760
Vítězslav Dvořák <info@vitexsoftware.cz>
2021-06-21 20:23:54 +0200

    package update

--- downloadExe.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DWLURL="https://mt.lv/winbox64"
-VERSION=`curl -s https://mikrotik.com/download | awk  '/WinBox ([0-9\.]*) \(64-bit\)/{print $3}'`
-dch --upstream --distribution stable --newversion $VERSION "package update with winbox.exe $VERSION"
-git commit -m "package update with winbox.exe $VERSION" debian/changelog
-wget $DWLURL
-
